74e9e3009f42b535ea380d48561c0083c61e4f49
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-04-26 23:37:54 +0100

    Install Grappelli admin

--- boardgame/boardgame/settings.py
+++ boardgame/boardgame/settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grappelli',
     'django.contrib.admin',
     'debug_toolbar',
     'south',
--- boardgame/boardgame/urls.py
+++ boardgame/boardgame/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^cards/', include('cards.urls', 'cards', 'cards')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'', include('game.urls', 'game', 'game')),
 )
