713d0eab3a623da3607d1af385872c58356eade6
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-04-28 13:19:51 +0100

    Move modifier table generation into a template tag

--- boardgame/cards/templatetags/modifier_tags.py
+++ boardgame/cards/templatetags/modifier_tags.py
@@ -4,6 +4,12 @@ from django.template.loader import get_template
 register = template.Library()
 
 
-@register.inclusion_tag('cards/_modifier_td.html')
-def modifier_td(modifier):
-    return {'modifier': modifier}
+@register.inclusion_tag('cards/_hero_component_table.html')
+def hero_component_table(components, attributes):
+    try:
+        is_iterable = iter(components)
+    except TypeError:
+        # if we were given a single instance of a component, we can just
+        # put it in a single-item list and then not worry about it
+        components = [components]
+    return {'components': components, 'attributes': attributes}
